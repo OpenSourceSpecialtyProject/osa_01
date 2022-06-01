6596ce3094daba61591ef68d900c75b5ff6ea213
Joon Hee <parkes1801@naver.com>
2022-06-01 15:34:24 +0900

    게시판조금수정

--- app/src/main/java/com/example/osa_01/Board.java
+++ app/src/main/java/com/example/osa_01/Board.java
@@ -4,6 +4,33 @@ public class Board {
     private String title;
     private String contents;
     private String Uid;
+    private int board_total;
+    private int board_current;
+    private String applycant;
+
+    public int getBoard_total() {
+        return board_total;
+    }
+
+    public void setBoard_total(int board_total) {
+        this.board_total = board_total;
+    }
+
+    public int getBoard_current() {
+        return board_current;
+    }
+
+    public void setBoard_current(int board_current) {
+        this.board_current = board_current;
+    }
+
+    public String getApplycant() {
+        return applycant;
+    }
+
+    public void setApplycant(String applycant) {
+        this.applycant = applycant;
+    }
 
     public Board() {}
 
--- app/src/main/java/com/example/osa_01/BoardInfoActivity.java
+++ app/src/main/java/com/example/osa_01/BoardInfoActivity.java
@@ -2,7 +2,9 @@ package com.example.osa_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BoardInfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class BoardInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_info);
+
+        Intent intent = getIntent();
+        String t1 = intent.getStringExtra("title");
+
+        TextView tv_item_title = (TextView) findViewById(R.id.board_item_title);
+        tv_item_title.setText(t1);
     }
 }
