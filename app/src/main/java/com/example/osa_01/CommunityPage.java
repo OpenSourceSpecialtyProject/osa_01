58205b1183098089b3e99072b64d45e3f070aabf
JUNWON BAEK <woonn97@gmail.com>
2022-06-01 13:19:41 +0900

    프론트엔드 작성 브랜치

--- app/src/main/java/com/example/osa_01/CommunityPage.java
+++ app/src/main/java/com/example/osa_01/CommunityPage.java
@@ -1,10 +1,18 @@
 package com.example.osa_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.annotation.NonNull;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class CommunityPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
