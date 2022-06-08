9ae35c74faa134f487d200828e303bd0235cce71
JUNWON BAEK <woonn97@gmail.com>
2022-06-08 15:19:37 +0900

    Login페이지 수정

--- app/src/main/java/com/example/osa_01/CommunityPage.java
+++ app/src/main/java/com/example/osa_01/CommunityPage.java
@@ -40,7 +40,7 @@ public class CommunityPage extends AppCompatActivity  {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),WritePostPage.class); //
+                Intent intent = new Intent(getApplicationContext(),WritePostPage.class); //버튼 클릭시
                 startActivityForResult(intent,222);
             }
         });
